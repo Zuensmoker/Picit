f35b6d921afa1b3e40a8cbeb4ec462a8cecad4f1
Jay <jay.vanluijn@student.hu.nl>
2022-10-05 14:26:38 +0200

    Inventory management

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main{
+
+    public static void main(String args[]){
+        Player p = new Player();
+
+        p.addItemToInv("Bacon");
+        p.printInv();
+        p.removeItemFromInv("Bacon");
+        p.printInv();
+    }
+}
